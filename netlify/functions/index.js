3ca3135793ec6907accd4a24cd8496fa5f1768d8
Amir Mousavi <a.mousavi@arasdp.ir>
2024-10-07 11:51:09 +0330

    fix netlify config

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,24 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
-const path = require('path');
-const app = express();
 const serverless = require('serverless-http');
-const port = process.env.PORT || 3000;
+const app = express();
 
 app.use(express.json());
-// Return index.html
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.post('/api/postal-code', async (req, res) => {
     const { postalCode } = req.body;
     try {
-        const response = await axios.post(process.env.PRIVATE_API_URL,  {
+        const response = await axios.post(process.env.PRIVATE_API_URL, {
             channel: "eShop",
             postal_code: postalCode
         });
         res.json(response.data);
     } catch (error) {
-        res.status(500).json({ error: 'An error occurred', message: error.message, meta: process.env.PRIVATE_API_URL });
+        res.status(500).json({ error: 'An error occurred' });
     }
 });
 
